cbc03968f108471fc0a2f057110bd1bc26c96fd1
Argenis Jesus Ferrer Mora <argenisf@gmail.com>
2016-11-22 16:56:18 +0100

    Set up basic functionality

--- app/src/main/java/com/example/argenisferrer/mpandroidsampleapp/MainActivity.java
+++ app/src/main/java/com/example/argenisferrer/mpandroidsampleapp/MainActivity.java
@@ -1,21 +1,62 @@
 package com.example.argenisferrer.mpandroidsampleapp;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
 import com.mixpanel.android.mpmetrics.MixpanelAPI;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String MIXPANEL_TOKEN = "f8bd7cddaf94642530004c3d0509691f";
+    private MixpanelAPI mixpanel;
     private Context mContext;
+    private JSONObject mCurrentProps;
+
+    private Button mMainButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mContext = this;
-        MixpanelAPI mixpanel = MixpanelAPI.getInstance(mContext, MIXPANEL_TOKEN);
+        mixpanel = MixpanelAPI.getInstance(mContext, MIXPANEL_TOKEN);
+        mixpanel.unregisterSuperProperty("loggedIn");
         mixpanel.track("App launched");
+
+        mMainButton = (Button)findViewById(R.id.btnMain);
+
+
+        mMainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Boolean loggedIn = false;
+                mCurrentProps = mixpanel.getSuperProperties();
+                try {
+                    if(mCurrentProps.has("loggedIn") && mCurrentProps.getBoolean("loggedIn")){
+                        loggedIn = true;
+                    }else{
+                        mCurrentProps.put("loggedIn", loggedIn);
+                    }
+                } catch (JSONException e) {}
+                Intent intent;
+                if(loggedIn){
+                    intent = new Intent(mContext, StickerActivity.class);
+                    Sticker mSticker = new Sticker(true);
+                    intent.putExtra("stickerId", mSticker.getSticketId());
+                }else{
+                    intent = new Intent(mContext, Auth.class);
+                }
+
+                startActivity(intent);
+            }
+        });
     }
 }
