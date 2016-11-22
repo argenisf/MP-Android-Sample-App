7852fdacf3032e09cf29fa4bbc1f31bae803504e
Argenis Jesus Ferrer Mora <argenisf@gmail.com>
2016-11-22 17:07:52 +0100

    Check for email validity during auth

--- app/src/main/java/com/example/argenisferrer/mpandroidsampleapp/Auth.java
+++ app/src/main/java/com/example/argenisferrer/mpandroidsampleapp/Auth.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -40,24 +41,33 @@ public class Auth extends AppCompatActivity {
             public void onClick(View v) {
 
                 if(mEmailInput.getText().toString().trim().length() > 0 && mNameInput.getText().toString().trim().length() > 0){
+                    if(isValidEmail(mEmailInput.getText().toString().trim())){
 
-                    //all checked, let's log in and proceed
-                    JSONObject props = new JSONObject();
-                    try {
-                        props.put("loggedIn",true);
-                    } catch (JSONException e) {}
-                    mixpanel.registerSuperProperties(props);
+                        //all checked, let's log in and proceed
+                        JSONObject props = new JSONObject();
+                        try {
+                            props.put("loggedIn",true);
+                        } catch (JSONException e) {}
+                        mixpanel.registerSuperProperties(props);
 
-                    Sticker sticker = new Sticker(true);
-                    Intent intent = new Intent(mContext, StickerActivity.class);
-                    intent.putExtra("stickerId",sticker.getSticketId());
-                    startActivity(intent);
-                    finish();
+                        Sticker sticker = new Sticker(true);
+                        Intent intent = new Intent(mContext, StickerActivity.class);
+                        intent.putExtra("stickerId",sticker.getSticketId());
+                        startActivity(intent);
+                        finish();
 
+                    }else{
+                        Toast.makeText(mContext,"Please enter a valid email address", Toast.LENGTH_LONG).show();
+                    }
                 }else{
                     Toast.makeText(mContext,"You need to input your email and name", Toast.LENGTH_LONG).show();
                 }
             }
         });
+    }
+
+
+    public final static boolean isValidEmail(String target) {
+        return !TextUtils.isEmpty(target) && android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
     }
 }
