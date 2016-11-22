07c5ace5583569e91849e9487ba55d3fa2ac381e
Argenis Jesus Ferrer Mora <argenisf@gmail.com>
2016-11-22 15:20:31 +0100

    Setting up Mixpanel
    Adding the library, enabling permissions and initializing the library
    for the first time.

--- app/src/main/java/com/example/argenisferrer/mpandroidsampleapp/MainActivity.java
+++ app/src/main/java/com/example/argenisferrer/mpandroidsampleapp/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.argenisferrer.mpandroidsampleapp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.mixpanel.android.mpmetrics.MixpanelAPI;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String MIXPANEL_TOKEN = "f8bd7cddaf94642530004c3d0509691f";
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mContext = this;
+        MixpanelAPI mixpanel = MixpanelAPI.getInstance(mContext, MIXPANEL_TOKEN);
+        mixpanel.track("App launched");
     }
 }
